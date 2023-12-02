5c6137567a249f49e37b50b972aa64e52c47fa7b
Pete Rice <peteza011@gmail.com>
2023-12-02 13:47:36 -0800

    initial commit

---
+++ SimpleColorApp/SimpleColorApp/SimpleColorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleColorAppApp.swift
+//  SimpleColorApp
+//
+//  Created by Peter Rice on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleColorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
