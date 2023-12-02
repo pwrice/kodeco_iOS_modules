f87262c784cc1a07796720a7136611341ae8cc0a
Pete Rice <peteza011@gmail.com>
2023-12-02 14:47:33 -0800

    adding code for part 2

--- SimpleColorApp/SimpleColorApp/ContentView.swift
+++ SimpleColorApp/SimpleColorApp/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  
+  @State private var redSliderValue: Double
+  @State private var greenSliderValue: Double
+  @State private var blueSliderValue: Double
+  @State private var boxColor:Color
+  
+  init() {
+    let defaultRed = 200.0
+    let defaultGreen = 100.0
+    let defaultBlue = 240.0
+    
+    self.redSliderValue = defaultRed
+    self.greenSliderValue = defaultGreen
+    self.blueSliderValue = defaultBlue
+    self.boxColor = ContentView.calculateBoxColor(red: defaultRed, green: defaultGreen, blue: defaultBlue)
+  }
+  
+  var body: some View {
+    VStack {
+      Text("Color Picker")
+        .font(.largeTitle)
+      RoundedRectangle(cornerRadius: 0)
+        .fill(boxColor)
+      Text("Red")
+      HStack {
+        Slider(value: $redSliderValue, in: 1.0...255.0)
+        Text(String(Int(redSliderValue.rounded())))
+      }
+      Text("Green")
+      HStack {
+        Slider(value: $greenSliderValue, in: 1.0...255.0)
+        Text(String(Int(greenSliderValue.rounded())))
+      }
+      Text("Blue")
+      HStack {
+        Slider(value: $blueSliderValue, in: 1.0...255.0)
+        Text(String(Int(blueSliderValue.rounded())))
+      }
+      Button("Set Color") {
+        boxColor = ContentView.calculateBoxColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue)
+      }
     }
+    .padding()
+  }
+  
+  private static func calculateBoxColor(red: Double, green: Double, blue: Double) -> Color {
+    Color(red: red/255.0, green: green/255.0, blue: blue/255.0)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
