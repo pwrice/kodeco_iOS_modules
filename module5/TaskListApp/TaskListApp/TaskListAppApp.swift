f682f06679f0539e985e76c66025aa5b2b6bb944
Pete Rice <peteza011@gmail.com>
2023-12-22 11:08:01 -0800

    TaskListApp initial commit

---
+++ module5/TaskListApp/TaskListApp/TaskListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListAppApp.swift
+//  TaskListApp
+//
+//  Created by Peter Rice on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
