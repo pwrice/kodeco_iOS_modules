686fcab6201f9b17bed670963a2fc5ae3d233f13
Pete Rice <peteza011@gmail.com>
2024-01-09 06:44:22 -0800

    cleaning up tests

--- module6/TaskListApp/TaskListAppTests/TaskCategoryViewModelTests.swift
+++ module6/TaskListApp/TaskListAppTests/TaskCategoryViewModelTests.swift
@@ -27,55 +27,49 @@ final class TaskCategoryViewModelTests: XCTestCase {
   }
   
   func testTaskListViewModelInit() throws {
-    XCTAssertEqual(taskListViewModel!.selectedCategory, nil)
-    XCTAssertEqual(taskListViewModel!.categories, tasksStore!.categories)
+    XCTAssertEqual(taskListViewModel!.selectedCategory, nil, "taskListViewModel.selectedCategory is initially nil but found \(String(describing: taskListViewModel!.selectedCategory))")
+    XCTAssertEqual(taskListViewModel!.categories, tasksStore!.categories, "taskListViewModel.categories initially should == tasksStore.categories but found \(taskListViewModel!.categories)")
   }
   
   func testTasksByCategory() throws {
-    XCTAssertEqual(taskListViewModel!.selectedCategory, nil)
-    var categoryTasks = taskListViewModel!.tasksForSelectedCategory
-    XCTAssertEqual(categoryTasks.count, 4)
-        
     for category in taskListViewModel!.categories {
       taskListViewModel!.selectedCategory = category
-      categoryTasks = taskListViewModel!.tasksForSelectedCategory
-      XCTAssertEqual(categoryTasks.count, 1)
-      XCTAssertEqual(categoryTasks.first?.category, category)
+      let categoryTasks = taskListViewModel!.tasksForSelectedCategory
+      XCTAssertEqual(categoryTasks.count, 1, "There should be 1 task for each category; found \(categoryTasks.count) ")
+      XCTAssertEqual(categoryTasks.first?.category, category, "The category shold match the selected category \(category); found \(String(describing: categoryTasks.first?.category))")
     }
   }
   
   func testTaskCategoryCounts() throws {
     for category in taskListViewModel!.categories {
       taskListViewModel!.selectedCategory = category
-      XCTAssertEqual(taskListViewModel!.taskCount(for: category), 1)
+      let taskCount = taskListViewModel!.taskCount(for: category)
+      XCTAssertEqual(taskCount, 1, "There should be 1 task for each category, found \(taskCount) for \(category)")
     }
   }
   
   func testTapCategoryUpdatesList() throws {
-    XCTAssertEqual(taskListViewModel!.selectedCategory, nil)
-    XCTAssertEqual(taskListViewModel!.tasksForSelectedCategory.count, 4)
-        
     for category in taskListViewModel!.categories {
       taskListViewModel!.tapCategory(category: category)
-      XCTAssertEqual(taskListViewModel!.selectedCategory, category)
+      XCTAssertEqual(taskListViewModel!.selectedCategory, category, "The selected category \(String(describing: taskListViewModel!.selectedCategory)) should match the tapped category \(category)")
       let tasksForCategory = taskListViewModel!.tasksForSelectedCategory
-      XCTAssertEqual(tasksForCategory.count, 1)
-      XCTAssertEqual(tasksForCategory.first?.category, category)
+      XCTAssertEqual(tasksForCategory.count, 1, "There should be 1 task for category \(category); found \(tasksForCategory.count) ")
+      XCTAssertEqual(tasksForCategory.first?.category, category, "The category should match the selected category \(category); found \(String(describing: tasksForCategory.first?.category))")
     }
   }
   
   func testTapCategoryTwiceResetsList() throws {
     // First tap
     taskListViewModel!.tapCategory(category: .home)
-    XCTAssertEqual(taskListViewModel!.selectedCategory, .home)
+    XCTAssertEqual(taskListViewModel!.selectedCategory, .home, "First tap should set selectec category to home, found \(String(describing: taskListViewModel!.selectedCategory))")
     var tasksForCategory = taskListViewModel!.tasksForSelectedCategory
-    XCTAssertEqual(tasksForCategory.count, 1)
-    XCTAssertEqual(tasksForCategory.first?.category, .home)
-    
+    XCTAssertEqual(tasksForCategory.count, 1, "There should be 1 task for the .home category; found \(tasksForCategory.count) ")
+    XCTAssertEqual(tasksForCategory.first?.category, .home, "The task.category should be .home; found \(String(describing: tasksForCategory.first?.category))")
+
     // Second tap
     taskListViewModel!.tapCategory(category: .home)
-    XCTAssertEqual(taskListViewModel!.selectedCategory, nil)
+    XCTAssertEqual(taskListViewModel!.selectedCategory, nil, "Second tap should set selectec category back to nil, found \(String(describing: taskListViewModel!.selectedCategory))")
     tasksForCategory = taskListViewModel!.tasksForSelectedCategory
-    XCTAssertEqual(tasksForCategory.count, 4)
+    XCTAssertEqual(tasksForCategory.count, 4, "All 4 tasks should be visible; found \(tasksForCategory.count)")
   }
 }
