56edbcac3c80132b6388952de1e188871b2a383f
Pete Rice <peteza011@gmail.com>
2024-02-28 06:26:32 -0800

    initial code commit

---
+++ module9/ImageSearch/ImageSearch/ImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSearchApp.swift
+//  ImageSearch
+//
+//  Created by Peter Rice on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
